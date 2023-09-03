a02702dc1a633aa8fbac46f13fc6ebcb35ed7242
nscanga <97981816+nscanga@users.noreply.github.com>
2023-09-03 00:42:42 -0300

    Se agregaron los Domain

---
+++ Domain/Usuario.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Email { get; set; }
+        public string Contraseña { get; set; }
+        public int PerfilId { get; set; }
+        public Perfil Perfil { get; set; }
+    }
+}
